0c4417df2834e31d6f4a7d91f8321dec4c0d9062
siva-reddy4827 <siva.reddy4827@gmail.com>
2022-07-19 18:44:10 +0530

    added document to project

--- rlpproject/src/main/java/com/rlp/project/rlpproject/RlpprojectApplication.java
+++ rlpproject/src/main/java/com/rlp/project/rlpproject/RlpprojectApplication.java
@@ -3,12 +3,11 @@ package com.rlp.project.rlpproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+//main class
 @SpringBootApplication
 public class RlpprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RlpprojectApplication.class, args);
 	}
-
 } 
