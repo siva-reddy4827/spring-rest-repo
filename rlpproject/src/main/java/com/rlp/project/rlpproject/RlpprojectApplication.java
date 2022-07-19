5a313b1fdc831cce3ef5f7801230e275fb310848
siva-reddy4827 <siva.reddy4827@gmail.com>
2022-07-19 18:50:48 +0530

    added doc

--- rlpproject/src/main/java/com/rlp/project/rlpproject/RlpprojectApplication.java
+++ rlpproject/src/main/java/com/rlp/project/rlpproject/RlpprojectApplication.java
@@ -3,7 +3,7 @@ package com.rlp.project.rlpproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+//add comments
 @SpringBootApplication
 public class RlpprojectApplication {
 
